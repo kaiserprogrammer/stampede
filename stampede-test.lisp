1692ef72e6c8d0d92d432aa0c96499ef9ce4982e
Jürgen Bickert <juergenbickert@gmail.com>
2012-07-16 14:20:18 +0200

    add url parsing

--- stampede-test.lisp
+++ stampede-test.lisp
@@ -23,30 +23,58 @@
             (declare (ignore stream))))))))
 
 (test http-protocol-reader
-  (is (equalp '((:method . "GET")
-                (:url . "/url")
-                (:version . "1.0")
-                ("blub" . "that"))
-              (with-input-from-string (s (format nil "GET /url HTTP/1.0~%blub: that~%~%"))
-                (http-protocol-reader s))))
-  (is (equalp '((:method . "GET")
-                (:url . "/url")
-                (:version . "1.0")
-                ("blub" . "that"))
-              (with-input-from-string (s (format nil "GET /url HTTP/1.0~%blub: that~%~%"))
-                (http-protocol-reader s)))))
+  (let ((req (with-input-from-string
+                 (s (format nil "GET /url HTTP/1.0~%blub: that~%~%"))
+               (http-protocol-reader s))))
+    (loop for (left right) in '((:method  "GET")
+                                (:url "/url")
+                                (:version "1.0")
+                                ("blub" "that"))
+       do (is (equal right
+                     (cdr (assoc left req :test #'equal))))))
+  (let ((req (with-input-from-string (s (format nil "GET /url HTTP/1.0~%blub: that~%~%"))
+               (http-protocol-reader s))))
+    (loop for (left right) in '((:method "GET")
+                                  (:url "/url")
+                                  (:version "1.0")
+                                  ("blub" "that"))
+       do (is (equal right
+                     (cdr (assoc left req :test #'equal)))))))
+
+(test url-parsing
+  (let* ((parsed-url (parse-url "/?john=doe"))
+        (url (elt parsed-url 0))
+         (params (elt parsed-url 1)))
+    (is (equal "/" url))
+    (is (equal "doe" (cdr (assoc "john" params :test #'equal)))))
+  (let* ((parsed-url (parse-url "/blub/this/that?john=doe&jonny=depp"))
+        (url (elt parsed-url 0))
+         (params (elt parsed-url 1)))
+    (is (equal "/blub/this/that" url))
+    (is (equal "doe" (cdr (assoc "john" params :test #'equal))))
+    (is (equal "depp" (cdr (assoc "jonny" params :test #'equal))))))
+
+(test query-parsing
+  (let ((params
+         (cdr (assoc
+               :params
+               (with-input-from-string (s (format nil "GET /?john=doe&jim=beam HTTP/1.0~%~%"))
+                 (http-protocol-reader s))))))
+    (is (equal "doe" (cdr (assoc "john" params :test #'equal))))
+    (is (equal "beam" (cdr (assoc "jim" params :test #'equal))))))
 
 (test http-protocol-writer
-  (is (equal "HTTP/1.1 200
+  (is (equal
+       "HTTP/1.1 200
 Content-Type: text/html; charset=utf-8
 
-blub
-"
-             (with-output-to-string (s)
-               (http-protocol-writer '((:status . 200)
-                                       (:version . "1.1")
-                                       ("Content-Type" . "text/html; charset=utf-8"))
-                                     "blub"
-                                     s)))))
+blub"
+       (with-output-to-string (s)
+         (http-protocol-writer
+          '((:status . 200)
+            (:version . "1.1")
+            ("Content-Type" . "text/html; charset=utf-8"))
+          "blub"
+          s)))))
 
 (run!)
--- stampede.lisp
+++ stampede.lisp
@@ -4,7 +4,8 @@
    :shutdown-server
    :create-server
    :http-protocol-reader
-   :http-protocol-writer))
+   :http-protocol-writer
+   :parse-url))
 (in-package :stampede)
 
 (defun create-server (host port handler &key (worker-threads 1))
@@ -46,10 +47,13 @@
                    (declare (ignore match))
                    groups))
          (method (svref groups 0))
-         (url (svref groups 1))
+         (parsed-url (parse-url (svref groups 1)))
+         (url (elt parsed-url 0))
+         (params (elt parsed-url 1))
          (http-protocol-version (svref groups 3)))
     (append (list (cons :method method)
                   (cons :url url)
+                  (cons :params params)
                   (cons :version http-protocol-version))
             (loop for line = (read-line stream)
                until (or (equal "" line)
@@ -57,11 +61,23 @@
                collect (let ((data (split ":" line)))
                          (cons (first data) (string-trim " " (second data))))))))
 (defun http-protocol-writer (data text stream)
-  (format stream "HTTP/~a ~a~%" (cdr (assoc :version data)) (cdr (assoc :status data)))
+  (format stream "HTTP/~a ~a~c~c" (cdr (assoc :version data)) (cdr (assoc :status data)) #\Return #\Newline)
   (loop for pair in data
      when (not
            (or (equal :version (car pair))
                (equal :status (car pair))))
-     do (format stream "~a: ~a~%" (car pair) (cdr pair)))
-  (format stream "~%")
-  (format stream "~a~%" text))
+     do (format stream "~a: ~a~c~c" (car pair) (cdr pair) #\Return #\Newline))
+  (format stream "~c~c" #\Return #\Newline)
+  (format stream "~a" text))
+
+(defun parse-url (url)
+  (let* ((splitted-url (split "\\?" url))
+         (url (elt splitted-url 0))
+         (query (when (> (length splitted-url) 1)
+                  (elt splitted-url 1))))
+    (let ((items (split "&" query)))
+      (list
+       url
+       (loop for item in items
+          for (left right) = (split "=" item)
+          collect (cons left right))))))
