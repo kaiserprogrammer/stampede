2a66a9dac7089f9b7114a668f1ea71c682c920e9
Jürgen Bickert <juergenbickert@gmail.com>
2014-01-21 03:03:32 +0100

    update convert from fiveam to lisp-unit2

--- stampede-test.lisp
+++ stampede-test.lisp
@@ -1,54 +1,51 @@
 (defpackage :stampede-test
-  (:use :cl :fiveam :stampede :usocket))
+  (:use :cl :lisp-unit2 :stampede :usocket))
 (in-package :stampede-test)
 
-(def-suite stampede)
-(in-suite stampede)
+(remove-tests)
 
-(test creating-closing-server
-  (finishes
+(define-test creating-closing-server ()
+  (let ((server (run-server 8080
+                            (lambda (stream)
+                              (declare (ignore stream))))))
+    (unwind-protect
+         (progn
+           (stop server)
+           (sleep 0.002)              ; wait for server to shutdown
+           (assert-error 'connection-refused-error
+             (with-client-socket (socket stream "127.0.0.1" 8080)
+               (declare (ignore stream)))))
+      (stop server))
     (let ((server (run-server 8080
                               (lambda (stream)
                                 (declare (ignore stream))))))
       (unwind-protect
            (progn
              (stop server)
-             (sleep 0.002)              ; wait for server to shutdown
-             (signals connection-refused-error
+             (sleep 0.002)            ; wait for server to shutdown
+             (assert-error 'connection-refused-error
                (with-client-socket (socket stream "127.0.0.1" 8080)
-                 (declare (ignore stream)))))
-        (stop server))
-      (let ((server (run-server 8080
-                                (lambda (stream)
-                                  (declare (ignore stream))))))
-        (unwind-protect
-             (progn
-               (stop server)
-               (sleep 0.002)            ; wait for server to shutdown
-               (signals connection-refused-error
-                 (with-client-socket (socket stream "127.0.0.1" 8080)
-                   (declare (ignore stream))))
-               (stop server))
-          (stop server))))))
-
-(test connecting-to-server
-  (finishes
-    (let* (answer
-           (server (run-server 8080
-                               (lambda (stream)
-                                 (setf answer (read-line stream))))))
-      (unwind-protect
-           (progn
-             (with-client-socket (socket stream "127.0.0.1" 8080)
-               (format stream "client says~%")
-               (force-output stream))
-             (sleep 0.004)
-             (is (string= "client says" answer))
-             (stop server)
-             (sleep 0.004))
+                 (declare (ignore stream))))
+             (stop server))
         (stop server)))))
 
-(test http-protocol-reader-get
+(define-test connecting-to-server ()
+  (let* (answer
+         (server (run-server 8080
+                             (lambda (stream)
+                               (setf answer (read-line stream))))))
+    (unwind-protect
+         (progn
+           (with-client-socket (socket stream "127.0.0.1" 8080)
+             (format stream "client says~%")
+             (force-output stream))
+           (sleep 0.004)
+           (assert-equal "client says" answer)
+           (stop server)
+           (sleep 0.004))
+      (stop server))))
+
+(define-test http-protocol-reader-get ()
   (let ((req (flexi-streams:with-input-from-sequence
                  (s (flexi-streams:string-to-octets (format nil "GET /url HTTP/1.0~%blub: that~%~%")))
                (http-protocol-reader s))))
@@ -58,19 +55,19 @@
                                 (:params nil)
                                 ("blub" "that"))
        do (progn
-            (is (assoc left req :test #'equal))
-            (is (equal right
-                       (cdr (assoc left req :test #'equal)))))))
+            (assert-true (assoc left req :test #'equal))
+            (assert-equal right
+                         (cdr (assoc left req :test #'equal))))))
   (let ((req (flexi-streams:with-input-from-sequence (s (flexi-streams:string-to-octets (format nil "GET /url HTTP/1.0~%blub: that~%~%")))
                (http-protocol-reader s))))
     (loop for (left right) in '((:method "GET")
                                 (:url "/url")
                                 (:version "1.0")
                                 ("blub" "that"))
-       do (is (equal right
-                     (cdr (assoc left req :test #'equal)))))))
+       do (assert-equal right
+                        (cdr (assoc left req :test #'equal))))))
 
-(test http-protocol-reader-post
+(define-test http-protocol-reader-post ()
   (let ((req (flexi-streams:with-input-from-sequence
                  (s (flexi-streams:string-to-octets (format nil "POST /authors HTTP/1.1
 Host: juergenbickert.de
@@ -83,13 +80,13 @@ name=Alan+Perlis&commit=Add+Author")))
                                 (:version "1.1")
                                 (:content-length 34)
                                 ("Host" "juergenbickert.de"))
-       do (is (equal right
-                     (cdr (assoc left req :test #'equal)))))
+       do (assert-equal right
+                       (cdr (assoc left req :test #'equal))))
     (let ((params (cdr (assoc :params req))))
-      (is (string= "Alan Perlis" (cdr (assoc "name" params :test #'string=))))
-      (is (string= "Add Author" (cdr (assoc "commit" params :test #'string=)))))))
+      (assert-equal "Alan Perlis" (cdr (assoc "name" params :test #'string=)))
+      (assert-equal "Add Author" (cdr (assoc "commit" params :test #'string=))))))
 
-(test http-protocol-reader-put
+(define-test http-protocol-reader-put ()
   (let ((req (flexi-streams:with-input-from-sequence
                  (s (flexi-streams:string-to-octets (format nil "POST /authors HTTP/1.1
 Host: juergenbickert.de
@@ -102,54 +99,55 @@ name=Alan+Perlis&commit=Add+Author&_method=put")))
                                 (:version "1.1")
                                 (:content-length 46)
                                 ("Host" "juergenbickert.de"))
-       do (is (equal right
-                     (cdr (assoc left req :test #'equal)))))
+       do (assert-equal right
+                        (cdr (assoc left req :test #'equal))))
     (let ((params (cdr (assoc :params req))))
-      (is (string= "Alan Perlis" (cdr (assoc "name" params :test #'string=))))
-      (is (string= "Add Author" (cdr (assoc "commit" params :test #'string=)))))))
+      (assert-equal "Alan Perlis" (cdr (assoc "name" params :test #'string=)))
+      (assert-equal "Add Author" (cdr (assoc "commit" params :test #'string=))))))
 
-(test url-parsing
+(define-test url-parsing ()
   (let* ((parsed-url (parse-url "/?john=doe"))
          (url (elt parsed-url 0))
          (params (elt parsed-url 1)))
-    (is (equal "/" url))
-    (is (equal "doe" (cdr (assoc "john" params :test #'equal)))))
+    (assert-equal "/" url)
+    (assert-equal "doe" (cdr (assoc "john" params :test #'equal))))
   (let* ((parsed-url (parse-url "/blub/this/that?john=doe&jonny=depp&encoded=%2F"))
          (url (elt parsed-url 0))
          (params (elt parsed-url 1)))
-    (is (equal "/blub/this/that" url))
-    (is (equal "doe" (cdr (assoc "john" params :test #'equal))))
-    (is (equal "depp" (cdr (assoc "jonny" params :test #'equal))))
-    (is (equal "/" (cdr (assoc "encoded" params :test #'string=))))))
+    (assert-equal "/blub/this/that" url)
+    (assert-equal "doe" (cdr (assoc "john" params :test #'equal)))
+    (assert-equal "depp" (cdr (assoc "jonny" params :test #'equal)))
+    (assert-equal "/" (cdr (assoc "encoded" params :test #'string=)))))
 
-(test query-parsing
+(define-test query-parsing ()
   (let ((params
          (cdr (assoc
                :params
                (flexi-streams:with-input-from-sequence (s (flexi-streams:string-to-octets (format nil "GET /?john=doe&jim=beam HTTP/1.0~%~%")))
                  (http-protocol-reader s))))))
-    (is (equal "doe" (cdr (assoc "john" params :test #'equal))))
-    (is (equal "beam" (cdr (assoc "jim" params :test #'equal))))))
+    (assert-equal "doe" (cdr (assoc "john" params :test #'equal)))
+    (assert-equal "beam" (cdr (assoc "jim" params :test #'equal)))))
 
-(test http-protocol-writer
-  (is (equal
-       "HTTP/1.1 200
+(define-test http-protocol-writing ()
+  (assert-equal
+   "HTTP/1.1 200
 Location: /path/to/
 Content-Type: text/html; charset=utf-8
 
 blub"
-       (flexi-streams:octets-to-string
-        (flexi-streams:with-output-to-sequence (s)
-          (http-protocol-writer
-           `(("Location" . "/path/to/")
-             (:status . 200)
-             (:version . "1.1")
-             (:stream . ,s)
-             ("Content-Type" . "text/html; charset=utf-8"))
-           "blub"
-           s))))))
+   (flexi-streams:octets-to-string
+    (flexi-streams:with-output-to-sequence (s)
+      (http-protocol-writer
+       `(("Location" . "/path/to/")
+         (:status . 200)
+         (:version . "1.1")
+         (:stream . ,s)
+         ("Content-Type" . "text/html; charset=utf-8"))
+       "blub"
+       s)))))
 
-(test url-decoding
-  (is (string= "http://www.test.org/John%202.jpg" (urlencode:urldecode "http%3A%2F%2Fwww.test.org%2FJohn%25202.jpg"))))
+(define-test url-decoding ()
+  (assert-equal "http://www.test.org/John%202.jpg" (urlencode:urldecode "http%3A%2F%2Fwww.test.org%2FJohn%25202.jpg")))
 
-(run!)
+(lisp-unit2:with-test-results ()
+  (run-tests))
